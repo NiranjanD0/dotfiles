0be2087fc93404aa52289f63ea0ffa1ed8f67056
NiranjanD0 <niranjan4281@gmail.com>
2025-10-24 02:57:12 +0530

    Added transparency in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " " -- Set space as the leader key
-
 vim.keymap.set("n","<leader>e",vim.cmd.Ex)
 
+--- SCRIPTURE OF TRANSPARENCY ---
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
