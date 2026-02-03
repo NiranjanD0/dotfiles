d87cb9aebdad7085768b366d54a5d81ebbe34670
NiranjanD0 <niranjan4281@gmail.com>
2026-02-03 17:34:11 +0530

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,3 +30,10 @@ if status is-interactive
     pfetch
 end
 
+
+# pnpm
+set -gx PNPM_HOME "/home/rio/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
