e6bdb329ce5564e4016840d74f3e9f6f49e17096
NiranjanD0 <niranjan4281@gmail.com>
2026-06-01 16:00:53 +0530

    updated links script

--- links.sh
+++ links.sh
@@ -1,9 +1,3 @@
-rm /home/rio/.config/fish  -rf
-rm /home/rio/.config/hypr  -rf
-rm /home/rio/.config/kitty  -rf
-rm /home/rio/.config/quickshell  -rf
-rm /home/rio/.config/wlogout  -rf
-
 ln -s /home/rio/dotfiles/fish/ /home/rio/.config/
 ln -s /home/rio/dotfiles/hypr/ /home/rio/.config/
 ln -s /home/rio/dotfiles/kitty/ /home/rio/.config/
